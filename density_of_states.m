4fc011dae3551ac40d2a70630b4a12ab234a8c45
Sandesh Kalantre <kalantresandesh@gmail.com>
2016-12-28 11:46:39 +0530

    generated figures for dos

--- density_of_states.m
+++ density_of_states.m
@@ -8,14 +8,14 @@
 % Andreev bound states visible in the gap for N_D > 10
 
 % eta : 0+ for calculating retarded Green's functions
-eta = 1e-8;
+eta = 1e-6;
     
 % number of points in the device (channel)
-N_D = 30;
+N_D = 100;
 
 % t0 : tight binding parameter = hbar^2/(2 m a^2)
 % t0 units : eV
-t0 = 1.0;
+t0 = 1;
 
 % Device Hamiltonian (BdG Hamiltonian in the transformed domain)
 % We assume mu = 0 and Delta = 0 in the device region
@@ -45,10 +45,6 @@ for ii = 1:N_D
        end    
 end
 
-% kT : k_B * Temperature(K)
-% kT units : eV
-% kT default value : 0.026 eV (Room temperature)
-kT = 0.026;
 
 % Delta: Superconducting order paramter
 % Delta units : eV
@@ -57,10 +53,10 @@ Delta = 0.01;
 % mu1 : electrochemical potential of the left contact
 mu1 = 0.0;
 % mu2 : electrochemical potential of the right contact
-mu2 = 0.3;
+mu2 = 0.0;
 
 % phi : phase difference between the two superconductors
-phi = 0.5*pi;
+phi = 0;
 
 Delta1 = Delta;
 alpha1 = [2*t0 - mu1 Delta1; conj(Delta1) -2*t0 + mu1];
@@ -71,14 +67,14 @@ alpha2 = [2*t0 - mu2 Delta2; conj(Delta2) -2*t0 + mu2];
 beta2 = -t0* [1 0; 0 -1];
 
 % N_E : number of points in the energy grid
-N_E = 1000;
-E_vec = 4* t0 .* linspace(-1,1,N_E);
+N_E = 500;
+E_vec = 2* Delta .* linspace(-1,1,N_E);
 
 %density of states
 A = zeros(1,length(E_vec));
 
 for ii = 1:length(E_vec)
-    E = E_vec(ii);
+    E = E_vec(ii)
 
     g1 = surface_g(E,alpha1,beta1,eta);
     g2 = surface_g(E,alpha2,beta2,eta);
@@ -90,19 +86,24 @@ for ii = 1:length(E_vec)
     Sigma1(2,2) = g1(2,2);
     
     Sigma2 = zeros(2*N_D);
-    Sigma2(2*N_D - 1,2*N_D - 1) = g1(1,1);
-    Sigma2(2*N_D - 1,2*N_D) = g1(1,2);
-    Sigma2(2*N_D,2*N_D - 1) = g1(2,1);
-    Sigma2(2*N_D,2*N_D) = g1(2,2);
+    Sigma2(2*N_D - 1,2*N_D - 1) = g2(1,1);
+    Sigma2(2*N_D - 1,2*N_D) = g2(1,2);
+    Sigma2(2*N_D,2*N_D - 1) = g2(2,1);
+    Sigma2(2*N_D,2*N_D) = g2(2,2);
     
     G_D = inv((E + 1i*eta) .* eye(2*N_D) - H_D - Sigma1 - Sigma2);
     
     A(ii) = trace(1j * (G_D - G_D'));
 end
 
+scaled_E_vec = E_vec./Delta;
 figure(1)
-plot(E_vec,A,'linewidth',1.5)
-xlabel('Energy ($\frac{E}{\Delta}$)','interpreter','latex','fontsize',16);
-ylabel('Density of States','interpreter','latex','fontsize',16);
-title(['DOS for number of device points : ' num2str(N_D)],'interpreter','latex','fontsize',16)
+str = strcat(['dos_' num2str(N_D) '_points.mat']);
+save(str,'scaled_E_vec','A');
+plot(scaled_E_vec,A,'linewidth',2.0)
+set(gca,'FontSize',20)
+ylim([0 1000])
+xlabel('Energy ($\frac{E}{\Delta}$)','interpreter','latex','fontsize',24);
+ylabel('Density of States','interpreter','latex','fontsize',24);
+title(['DOS for number of device points : ' num2str(N_D)],'interpreter','latex','fontsize',24)
 
